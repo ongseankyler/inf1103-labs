2f4d5c884fa82bab18ff46e75d450cc4af2a4aec
ongseankyler <ongseankyler@gmail.com>
2026-09-04 17:07:11 +0800

    fourth post

--- hello.py
+++ hello.py
@@ -3,7 +3,19 @@ print("Welcome here")
 print("My first post!")
 print("=====================================")
 
-username = "cool_creator"
+username = input("Enter Username ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+
+print("\n Instagram Profile")
+print("=====================================")
+print("Username: ", username)
+print("Age: ", age)
+print("Category: ", category)
+
+
+
 bio = "Funn blogger"
 followers = 100
 
